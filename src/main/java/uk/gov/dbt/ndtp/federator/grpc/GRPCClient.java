d4b083893a484ad1eab1a898766e5ad5bb88ce74
Sumeet Raheja <sumeet.raheja@informed.com>
2025-08-20 09:49:25 +0100

    DPAV-1424 Added MTLS support for both Client and Server

--- src/main/java/uk/gov/dbt/ndtp/federator/grpc/GRPCClient.java
+++ src/main/java/uk/gov/dbt/ndtp/federator/grpc/GRPCClient.java
@@ -26,18 +26,8 @@
 
 package uk.gov.dbt.ndtp.federator.grpc;
 
-import static uk.gov.dbt.ndtp.federator.utils.GRPCExceptionUtils.handleGRPCException;
-
-import io.grpc.Context;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import io.grpc.Status;
-import io.grpc.StatusRuntimeException;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
+import io.grpc.*;
+import lombok.SneakyThrows;
 import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.utils.Bytes;
 import org.slf4j.Logger;
@@ -47,15 +37,19 @@ import uk.gov.dbt.ndtp.federator.exceptions.RetryableException;
 import uk.gov.dbt.ndtp.federator.utils.KafkaUtil;
 import uk.gov.dbt.ndtp.federator.utils.PropertyUtil;
 import uk.gov.dbt.ndtp.federator.utils.RedisUtil;
-import uk.gov.dbt.ndtp.grpc.API;
-import uk.gov.dbt.ndtp.grpc.APITopics;
-import uk.gov.dbt.ndtp.grpc.FederatorServiceGrpc;
-import uk.gov.dbt.ndtp.grpc.KafkaByteBatch;
-import uk.gov.dbt.ndtp.grpc.TopicRequest;
+import uk.gov.dbt.ndtp.grpc.*;
 import uk.gov.dbt.ndtp.secure.agent.sources.Event;
 import uk.gov.dbt.ndtp.secure.agent.sources.Header;
 import uk.gov.dbt.ndtp.secure.agent.sources.kafka.sinks.KafkaSink;
 import uk.gov.dbt.ndtp.secure.agent.sources.memory.SimpleEvent;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static uk.gov.dbt.ndtp.federator.utils.GRPCExceptionUtils.handleGRPCException;
 
 /**
  * GRPCClient is a client for the FederatorService GRPC service.
@@ -68,178 +62,203 @@ import uk.gov.dbt.ndtp.secure.agent.sources.memory.SimpleEvent;
  * It is also used to consume messages and send them to the KafkaSink.
  */
 public class GRPCClient implements AutoCloseable {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger("GRPClient");
-    private static final String CLIENT_KEEP_ALIVE_TIME = "client.keepAliveTime.secs";
-    private static final String CLIENT_KEEP_ALIVE_TIMEOUT = "client.keepAliveTimeout.secs";
-    private static final String CLIENT_IDLE_TIMEOUT = "client.idleTimeout.secs";
-    private static final String TEN = "10";
-    private static final String THIRTY = "30";
-
-    private final ManagedChannel channel;
-    private final FederatorServiceGrpc.FederatorServiceBlockingStub blockingStub;
-    private final String client;
-    private final String key;
-    private final String topicPrefix;
-    private final String serverName;
-
-    public GRPCClient(ConnectionProperties connectionProperties, String topicPrefix) {
-        this(
-                connectionProperties.clientName(),
-                connectionProperties.clientKey(),
-                connectionProperties.serverName(),
-                connectionProperties.serverHost(),
-                connectionProperties.serverPort(),
-                connectionProperties.tls(),
-                topicPrefix);
-    }
-
-    public GRPCClient(
-            String client,
-            String key,
-            String serverName,
-            String host,
-            int port,
-            boolean isTLSEnabled,
-            String topicPrefix) {
-        LOGGER.info("Topic Prefix - '{}'", topicPrefix);
-        this.topicPrefix = topicPrefix;
-        LOGGER.info("Client Name - '{}'", client);
-        this.client = client;
-        LOGGER.info("Client Key is empty string - '{}'", key.isEmpty());
-        this.key = key;
-        LOGGER.info("GRPC Server Name - '{}'", serverName);
-        this.serverName = serverName;
-        LOGGER.info("GRPC Server Host - '{}'", host);
-        LOGGER.info("GRPC Server Port - '{}'", port);
-        LOGGER.info("GRPC Server TLS Enabled - '{}'", isTLSEnabled);
-        channel = generateChannel(host, port, isTLSEnabled);
-        blockingStub = FederatorServiceGrpc.newBlockingStub(channel);
-    }
-
-    public String getRedisPrefix() {
-        return client + "-" + serverName;
-    }
-
-    @Override
-    public void close() {
-        try {
-            channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-        } catch (Throwable t) {
-            LOGGER.error("Exception closing client", t);
-        }
-    }
-
-    public List<String> obtainTopics() {
-        try {
-            LOGGER.info("Making call to obtain topics for client: '{}'.", client);
-            API apiRequest = API.newBuilder().setClient(client).setKey(key).build();
-            LOGGER.info("built apiRequest for client grpc call");
-            APITopics apiTopicsResponse = blockingStub.getKafkaTopics(apiRequest);
-            LOGGER.info("Received response: {}", apiTopicsResponse.getTopicsList());
-            return apiTopicsResponse.getTopicsList();
-        } catch (StatusRuntimeException exception) {
-            String msg = String.format("Unable to obtain topics: %s", exception.getMessage());
-            LOGGER.warn(msg);
-            handleGRPCException(exception);
-        } catch (Exception exception) {
-            String msg = String.format("Encountered error obtaining topics: %s", exception.getMessage());
-            LOGGER.error(msg);
-            throw exception;
-        }
-        return Collections.emptyList();
-    }
-
-    public void processTopic(String topic, long offset) {
-        LOGGER.info("Processing topic: {}", topic);
-        // This does a smoke test so hopefully fail early if problems.
-        RedisUtil.getInstance();
-        TopicRequest topicRequest = TopicRequest.newBuilder()
-                .setTopic(topic)
-                .setOffset(offset)
-                .setAPIKey(key)
-                .setClient(client)
-                .build();
-
-        try (KafkaSink<Bytes, Bytes> sink = getSender(topic, this.topicPrefix, this.serverName)) {
-            try (Context.CancellableContext withCancellation = Context.current().withCancellation()) {
-                withCancellation.run(() -> consumeMessagesAndSendOn(topicRequest, sink));
-            } catch (StatusRuntimeException exception) {
-                if (Status.INVALID_ARGUMENT
-                        .getCode()
-                        .equals(exception.getStatus().getCode())) {
-                    LOGGER.error("Topic ({}) no longer valid for client ({})", topic, client);
-                } else {
-                    LOGGER.error("Topic processing stopped due to unknown error.", exception);
-                }
-            } catch (Exception exception) {
-                LOGGER.error("Topic processing stopped due to error.", exception);
-            }
-        } catch (KafkaException e) {
-            LOGGER.warn("Failed to create KafkaSink - '{}'", e.getMessage());
-            throw new RetryableException(e);
-        }
-    }
-
-    public void consumeMessagesAndSendOn(TopicRequest topicRequest, KafkaSink<Bytes, Bytes> sink) {
-        Iterator<KafkaByteBatch> iterator = blockingStub.getKafkaConsumer(topicRequest);
-        while (iterator.hasNext()) {
-            KafkaByteBatch batch = iterator.next();
-            if (null == batch) {
-                LOGGER.info("Processing null message");
-                continue;
-            }
-            LOGGER.debug(
-                    "Consuming message: {}, {} : {}",
-                    batch.getTopic(),
-                    batch.getOffset(),
-                    batch.getValue().toStringUtf8());
-            sendMessage(sink, batch);
-            RedisUtil.getInstance().setOffset(getRedisPrefix(), topicRequest.getTopic(), batch.getOffset());
-            LOGGER.debug("Writing to redis: {}-{} {}", getRedisPrefix(), topicRequest.getTopic(), batch.getOffset());
-        }
-        LOGGER.info("Finished consuming");
-    }
-
-    public static void sendMessage(KafkaSink<Bytes, Bytes> sink, KafkaByteBatch batch) {
-        LOGGER.debug("Creating message to send");
-        Bytes key = new Bytes(batch.getKey().toByteArray());
-        Bytes value = new Bytes(batch.getValue().toByteArray());
-        List<Header> headers = batch.getSharedList().stream()
-                .map(h -> new Header(h.getKey(), h.getValue()))
-                .collect(Collectors.toList());
-        Event<Bytes, Bytes> event = new SimpleEvent<>(headers, key, value);
-        LOGGER.debug("Sending message");
-        sink.send(event);
-        LOGGER.debug("Sent event");
-    }
-
-    public static KafkaSink<Bytes, Bytes> getSender(String topic, String topicPrefix, String serverName) {
-        return KafkaUtil.getKafkaSink(concatCompoundTopicName(topic, topicPrefix, serverName));
-    }
-
-    private static String concatCompoundTopicName(String topic, String topicPrefix, String serverName) {
-        if (topicPrefix.isEmpty()) {
-            return String.join("-", serverName, topic);
-        }
-        return String.join("-", topicPrefix, serverName, topic);
-    }
-
-    public static ManagedChannel generateChannel(String host, int port, boolean isTLSEnabled) {
-        ManagedChannelBuilder<?> builder = ManagedChannelBuilder.forAddress(host, port)
-                .keepAliveTime(PropertyUtil.getPropertyIntValue(CLIENT_KEEP_ALIVE_TIME, THIRTY), TimeUnit.SECONDS)
-                .keepAliveTimeout(PropertyUtil.getPropertyIntValue(CLIENT_KEEP_ALIVE_TIMEOUT, TEN), TimeUnit.SECONDS)
-                .idleTimeout(PropertyUtil.getPropertyIntValue(CLIENT_IDLE_TIMEOUT, TEN), TimeUnit.SECONDS);
-        if (isTLSEnabled) {
-            builder.useTransportSecurity();
-        } else {
-            builder.usePlaintext();
-        }
-        builder.intercept(new CustomClientInterceptor());
-        return builder.build();
-    }
-
-    public void testConnectivity() {
-        KafkaUtil.getKafkaSinkBuilder().topic("test").build().close();
-    }
+	
+	public static final String CLIENT_CERT_CHAIN_FILE_PATH = "client.certChainFile";
+	public static final String CLIENT_PRIVATE_KEY_FILE_PATH = "client.privateKeyFile";
+	public static final String CLIENT_CA_PEM_FILE_PATH = "client.caCertFile";
+	private static final Logger LOGGER = LoggerFactory.getLogger("GRPClient");
+	private static final String CLIENT_KEEP_ALIVE_TIME = "client.keepAliveTime.secs";
+	private static final String CLIENT_KEEP_ALIVE_TIMEOUT = "client.keepAliveTimeout.secs";
+	private static final String CLIENT_IDLE_TIMEOUT = "client.idleTimeout.secs";
+	private static final String TEN = "10";
+	private static final String THIRTY = "30";
+	private final ManagedChannel channel;
+	private final FederatorServiceGrpc.FederatorServiceBlockingStub blockingStub;
+	private final String client;
+	private final String key;
+	private final String topicPrefix;
+	private final String serverName;
+	
+	public GRPCClient(ConnectionProperties connectionProperties, String topicPrefix) {
+		this(
+			connectionProperties.clientName(),
+			connectionProperties.clientKey(),
+			connectionProperties.serverName(),
+			connectionProperties.serverHost(),
+			connectionProperties.serverPort(),
+			connectionProperties.tls(),
+			topicPrefix);
+	}
+	
+	public GRPCClient(
+		String client, String key, String serverName, String host, int port, boolean isTLSEnabled, String topicPrefix) {
+		LOGGER.info("Initializing GRPCClient with client={}, serverName={}, host={}, port={}, isTLSEnabled={}, topicPrefix={}",
+			client, serverName, host, port, isTLSEnabled, topicPrefix);
+		
+		this.topicPrefix = topicPrefix;
+		this.client = client;
+		this.key = key;
+		this.serverName = serverName;
+		if (isTLSEnabled) {
+			LOGGER.info("Using TLS for GRPC connection");
+			channel = generateSecureChannel(host, port, generateChannelCredentials());
+		} else {
+			LOGGER.info("Using plaintext for GRPC connection");
+			channel = generateChannel(host, port);
+		}
+		
+		
+		blockingStub = FederatorServiceGrpc.newBlockingStub(channel);
+	}
+	
+	public static void sendMessage(KafkaSink<Bytes, Bytes> sink, KafkaByteBatch batch) {
+		LOGGER.debug("Creating message to send");
+		Bytes key = new Bytes(batch.getKey().toByteArray());
+		Bytes value = new Bytes(batch.getValue().toByteArray());
+		List<Header> headers = batch.getSharedList().stream()
+			.map(h -> new Header(h.getKey(), h.getValue()))
+			.collect(Collectors.toList());
+		Event<Bytes, Bytes> event = new SimpleEvent<>(headers, key, value);
+		LOGGER.debug("Sending message");
+		sink.send(event);
+		LOGGER.debug("Sent event");
+	}
+	
+	public static KafkaSink<Bytes, Bytes> getSender(String topic, String topicPrefix, String serverName) {
+		return KafkaUtil.getKafkaSink(concatCompoundTopicName(topic, topicPrefix, serverName));
+	}
+	
+	private static String concatCompoundTopicName(String topic, String topicPrefix, String serverName) {
+		if (topicPrefix.isEmpty()) {
+			return String.join("-", serverName, topic);
+		}
+		return String.join("-", topicPrefix, serverName, topic);
+	}
+	
+	public static ManagedChannel generateChannel(String host, int port) {
+		ManagedChannelBuilder<?> builder = ManagedChannelBuilder.forAddress(host, port)
+			.keepAliveTime(PropertyUtil.getPropertyIntValue(CLIENT_KEEP_ALIVE_TIME, THIRTY), TimeUnit.SECONDS)
+			.keepAliveTimeout(PropertyUtil.getPropertyIntValue(CLIENT_KEEP_ALIVE_TIMEOUT, TEN), TimeUnit.SECONDS)
+			.idleTimeout(PropertyUtil.getPropertyIntValue(CLIENT_IDLE_TIMEOUT, TEN), TimeUnit.SECONDS);
+		
+		builder.usePlaintext();
+		builder.intercept(new CustomClientInterceptor());
+		
+		return builder.build();
+	}
+	
+	public static ManagedChannel generateSecureChannel(String host, int port, ChannelCredentials cred) {
+		ManagedChannelBuilder<?> builder = Grpc.newChannelBuilderForAddress(host, port, cred)
+			.keepAliveTime(PropertyUtil.getPropertyIntValue(CLIENT_KEEP_ALIVE_TIME, THIRTY), TimeUnit.SECONDS)
+			.keepAliveTimeout(PropertyUtil.getPropertyIntValue(CLIENT_KEEP_ALIVE_TIMEOUT, TEN), TimeUnit.SECONDS)
+			.idleTimeout(PropertyUtil.getPropertyIntValue(CLIENT_IDLE_TIMEOUT, TEN), TimeUnit.SECONDS);
+		
+		builder.intercept(new CustomClientInterceptor());
+		return builder.build();
+	}
+	
+	@SneakyThrows
+	private ChannelCredentials generateChannelCredentials() {
+		// Extract the file paths from properties
+		String certChainFilePath = PropertyUtil.getPropertyValue(CLIENT_CERT_CHAIN_FILE_PATH);
+		String clientPrivateKeyFilePath = PropertyUtil.getPropertyValue(CLIENT_PRIVATE_KEY_FILE_PATH);
+		String rootCertPath = PropertyUtil.getPropertyValue(CLIENT_CA_PEM_FILE_PATH);
+		LOGGER.info("Generating channel credentials with certChainFilePath: {}, clientPrivateKeyFilePath: {}, rootCertPath: {}",
+			certChainFilePath, clientPrivateKeyFilePath, rootCertPath);
+		
+		return TlsChannelCredentials.newBuilder()
+			.keyManager(getClass().getResourceAsStream(certChainFilePath), getClass().getResourceAsStream(clientPrivateKeyFilePath))
+			.trustManager(getClass().getResourceAsStream(rootCertPath))
+			.build();
+		
+		
+	}
+	
+	public String getRedisPrefix() {
+		return client + "-" + serverName;
+	}
+	
+	@Override
+	public void close() {
+		try {
+			channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+		} catch (Throwable t) {
+			LOGGER.error("Exception closing client", t);
+		}
+	}
+	
+	public List<String> obtainTopics() {
+		try {
+			LOGGER.info("Making call to obtain topics for client: '{}'.", client);
+			API apiRequest = API.newBuilder().setClient(client).setKey(key).build();
+			LOGGER.info("built apiRequest for client grpc call");
+			APITopics apiTopicsResponse = blockingStub.getKafkaTopics(apiRequest);
+			LOGGER.info("Received response: {}", apiTopicsResponse.getTopicsList());
+			return apiTopicsResponse.getTopicsList();
+		} catch (StatusRuntimeException exception) {
+			String msg = String.format("Unable to obtain topics: %s", exception.getMessage());
+			LOGGER.warn(msg);
+			handleGRPCException(exception);
+		} catch (Exception exception) {
+			String msg = String.format("Encountered error obtaining topics: %s", exception.getMessage());
+			LOGGER.error(msg);
+			throw exception;
+		}
+		return Collections.emptyList();
+	}
+	
+	public void processTopic(String topic, long offset) {
+		LOGGER.info("Processing topic: {}", topic);
+		// This does a smoke test so hopefully fail early if problems.
+		RedisUtil.getInstance();
+		TopicRequest topicRequest = TopicRequest.newBuilder()
+			.setTopic(topic)
+			.setOffset(offset)
+			.setAPIKey(key)
+			.setClient(client)
+			.build();
+		
+		try (KafkaSink<Bytes, Bytes> sink = getSender(topic, this.topicPrefix, this.serverName)) {
+			try (Context.CancellableContext withCancellation = Context.current().withCancellation()) {
+				withCancellation.run(() -> consumeMessagesAndSendOn(topicRequest, sink));
+			} catch (StatusRuntimeException exception) {
+				if (Status.INVALID_ARGUMENT
+					.getCode()
+					.equals(exception.getStatus().getCode())) {
+					LOGGER.error("Topic ({}) no longer valid for client ({})", topic, client);
+				} else {
+					LOGGER.error("Topic processing stopped due to unknown error.", exception);
+				}
+			} catch (Exception exception) {
+				LOGGER.error("Topic processing stopped due to error.", exception);
+			}
+		} catch (KafkaException e) {
+			LOGGER.warn("Failed to create KafkaSink - '{}'", e.getMessage());
+			throw new RetryableException(e);
+		}
+	}
+	
+	public void consumeMessagesAndSendOn(TopicRequest topicRequest, KafkaSink<Bytes, Bytes> sink) {
+		Iterator<KafkaByteBatch> iterator = blockingStub.getKafkaConsumer(topicRequest);
+		while (iterator.hasNext()) {
+			KafkaByteBatch batch = iterator.next();
+			if (null == batch) {
+				LOGGER.info("Processing null message");
+				continue;
+			}
+			LOGGER.debug(
+				"Consuming message: {}, {} : {}",
+				batch.getTopic(),
+				batch.getOffset(),
+				batch.getValue().toStringUtf8());
+			sendMessage(sink, batch);
+			RedisUtil.getInstance().setOffset(getRedisPrefix(), topicRequest.getTopic(), batch.getOffset());
+			LOGGER.debug("Writing to redis: {}-{} {}", getRedisPrefix(), topicRequest.getTopic(), batch.getOffset());
+		}
+		LOGGER.info("Finished consuming");
+	}
+	
+	public void testConnectivity() {
+		KafkaUtil.getKafkaSinkBuilder().topic("test").build().close();
+	}
 }
--- src/main/java/uk/gov/dbt/ndtp/federator/grpc/GRPCServer.java
+++ src/main/java/uk/gov/dbt/ndtp/federator/grpc/GRPCServer.java
@@ -26,18 +26,18 @@
 
 package uk.gov.dbt.ndtp.federator.grpc;
 
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
-import io.grpc.ServerInterceptors;
-import java.io.IOException;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
+import io.grpc.*;
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.gov.dbt.ndtp.federator.utils.ClientFilter;
 import uk.gov.dbt.ndtp.federator.utils.PropertyUtil;
 import uk.gov.dbt.ndtp.federator.utils.ThreadUtil;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 /**
  * GRPCServer is a server for the FederatorService GRPC service.
@@ -46,59 +46,97 @@ import uk.gov.dbt.ndtp.federator.utils.ThreadUtil;
  * It is also used to close the GRPC server.
  */
 public class GRPCServer implements AutoCloseable {
-
-    public static final String SERVER_PORT = "server.port";
-    public static final String DEFAULT_PORT = "8080";
-    public static final String SERVER_KEEP_ALIVE_TIME = "server.keepAliveTime";
-    public static final String SERVER_KEEP_ALIVE_TIMEOUT = "server.keepAliveTimeout";
-    public static final String SERVER_TLS_ENABLED = "server.tlsEnabled";
-    public static final String SERVER_CERT_CHAIN_FILE = "server.certChainFile";
-    public static final String SERVER_PRIVATE_KEY_FILE = "server.privateKeyFile";
-    public static final String FIVE = "5";
-    public static final String ONE = "1";
-    public static final String FALSE = "false";
-
-    public static final Logger LOGGER = LoggerFactory.getLogger("GRPCServer");
-
-    private final Server server;
-
-    public GRPCServer(List<ClientFilter> filters, Set<String> sharedHeaders) {
-        server = generateServer(filters, sharedHeaders);
-    }
-
-    public static Server generateServer(List<ClientFilter> filters, Set<String> sharedHeaders) {
-        ServerBuilder<?> builder = ServerBuilder.forPort(PropertyUtil.getPropertyIntValue(SERVER_PORT, DEFAULT_PORT))
-                .executor(ThreadUtil.threadExecutor("GRPCServer"))
-                .keepAliveTime(PropertyUtil.getPropertyIntValue(SERVER_KEEP_ALIVE_TIME, FIVE), TimeUnit.SECONDS)
-                .keepAliveTimeout(PropertyUtil.getPropertyIntValue(SERVER_KEEP_ALIVE_TIMEOUT, ONE), TimeUnit.SECONDS);
-        builder.addService(ServerInterceptors.intercept(
-                new GRPCFederatorService(filters, sharedHeaders), new CustomServerInterceptor()));
-
-        if (PropertyUtil.getPropertyBooleanValue(SERVER_TLS_ENABLED, FALSE)) {
-            builder.useTransportSecurity(
-                    PropertyUtil.getPropertyFileValue(SERVER_CERT_CHAIN_FILE),
-                    PropertyUtil.getPropertyFileValue(SERVER_PRIVATE_KEY_FILE));
-        }
-        return builder.build();
-    }
-
-    public void start() {
-        try {
-            LOGGER.info("GRPCServer starting");
-            server.start();
-        } catch (IOException e) {
-            LOGGER.error("Exception encountered starting GRPC Server", e);
-        }
-    }
-
-    @Override
-    public void close() throws Exception {
-        try {
-            LOGGER.info("GRPCServer close called");
-            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
-            LOGGER.info("GRPCServer closed");
-        } catch (InterruptedException e) {
-            LOGGER.error("Exception occurred during shutdown", e);
-        }
-    }
+	
+	public static final String SERVER_PORT = "server.port";
+	public static final String DEFAULT_PORT = "8080";
+	public static final String SERVER_KEEP_ALIVE_TIME = "server.keepAliveTime";
+	public static final String SERVER_KEEP_ALIVE_TIMEOUT = "server.keepAliveTimeout";
+	public static final String SERVER_TLS_ENABLED = "server.tlsEnabled";
+	public static final String SERVER_CERT_CHAIN_FILE_PATH = "server.certChainFile";
+	public static final String SERVER_PRIVATE_KEY_FILE_PATH = "server.privateKeyFile";
+	public static final String SERVER_CA_PEM_FILE_PATH = "server.caPem";
+	public static final String FIVE = "5";
+	public static final String ONE = "1";
+	public static final String TRUE = "true";
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger("GRPCServer");
+	
+	private final Server server;
+	
+	private ServerCredentials creds;
+	
+	public GRPCServer(List<ClientFilter> filters, Set<String> sharedHeaders) {
+		if (PropertyUtil.getPropertyBooleanValue(SERVER_TLS_ENABLED, TRUE)) {
+			creds = generateServerCredentials();
+			server = generateServer(creds, filters, sharedHeaders);
+		} else {
+			LOGGER.warn("Server TLS is not enabled, using insecure server.");
+			server = generateServer(filters, sharedHeaders);
+		}
+	}
+	
+	public static Server generateServer(ServerCredentials creds, List<ClientFilter> filters, Set<String> sharedHeaders) {
+		
+		ServerBuilder<?> builder = Grpc.newServerBuilderForPort(PropertyUtil.getPropertyIntValue(SERVER_PORT, DEFAULT_PORT), creds)
+			.executor(ThreadUtil.threadExecutor("GRPCServer"))
+			.keepAliveTime(PropertyUtil.getPropertyIntValue(SERVER_KEEP_ALIVE_TIME, FIVE), TimeUnit.SECONDS)
+			.keepAliveTimeout(PropertyUtil.getPropertyIntValue(SERVER_KEEP_ALIVE_TIMEOUT, ONE), TimeUnit.SECONDS);
+		
+		builder.addService(ServerInterceptors.intercept(
+			new GRPCFederatorService(filters, sharedHeaders), new CustomServerInterceptor()));
+		
+		return builder.build();
+	}
+	
+	public static Server generateServer(List<ClientFilter> filters, Set<String> sharedHeaders) {
+		ServerBuilder<?> builder = ServerBuilder.forPort(PropertyUtil.getPropertyIntValue(SERVER_PORT, DEFAULT_PORT))
+			.executor(ThreadUtil.threadExecutor("GRPCServer"))
+			.keepAliveTime(PropertyUtil.getPropertyIntValue(SERVER_KEEP_ALIVE_TIME, FIVE), TimeUnit.SECONDS)
+			.keepAliveTimeout(PropertyUtil.getPropertyIntValue(SERVER_KEEP_ALIVE_TIMEOUT, ONE), TimeUnit.SECONDS);
+		builder.addService(ServerInterceptors.intercept(
+			new GRPCFederatorService(filters, sharedHeaders), new CustomServerInterceptor()));
+		
+		return builder.build();
+	}
+	
+	@SneakyThrows
+	private ServerCredentials generateServerCredentials() {
+		
+		String certChainFilePath = PropertyUtil.getPropertyValue(SERVER_CERT_CHAIN_FILE_PATH);
+		String serverPrivateKeyFilePath = PropertyUtil.getPropertyValue(SERVER_PRIVATE_KEY_FILE_PATH);
+		String rootCertPath = PropertyUtil.getPropertyValue(SERVER_CA_PEM_FILE_PATH);
+		LOGGER.info("Generating server credentials with certChainFilePath: {}, serverPrivateKeyFilePath: {}, rootCertPath: {}",
+			certChainFilePath, serverPrivateKeyFilePath, rootCertPath);
+		
+		if (certChainFilePath == null || serverPrivateKeyFilePath == null || rootCertPath == null) {
+			LOGGER.error("Server certificate chain, private key, or CA PEM file path is not set.");
+			throw new IllegalArgumentException("Server certificate chain, private key, or CA PEM file path is not set.");
+		}
+		TlsServerCredentials.Builder tlsBuilder = TlsServerCredentials.newBuilder()
+			.keyManager(getClass().getResourceAsStream(certChainFilePath), getClass().getResourceAsStream(serverPrivateKeyFilePath))
+			.trustManager(getClass().getResourceAsStream(rootCertPath))
+			.clientAuth(TlsServerCredentials.ClientAuth.REQUIRE);
+		
+		return tlsBuilder.build();
+	}
+	
+	public void start() {
+		try {
+			LOGGER.info("GRPCServer starting");
+			server.start();
+		} catch (IOException e) {
+			LOGGER.error("Exception encountered starting GRPC Server", e);
+		}
+	}
+	
+	@Override
+	public void close() throws Exception {
+		try {
+			LOGGER.info("GRPCServer close called");
+			server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+			LOGGER.info("GRPCServer closed");
+		} catch (InterruptedException e) {
+			LOGGER.error("Exception occurred during shutdown", e);
+		}
+	}
 }
