8b1536588040d28bac0ce4ec6e990dbc729f083a
cruddasj <james.cruddas@informed.com>
2025-09-02 19:38:31 +0100

    fix(DPAV-1644): lint and remove dummy token from prior experiment

--- src/main/java/uk/gov/dbt/ndtp/federator/service/IdpTokenServiceImpl.java
+++ src/main/java/uk/gov/dbt/ndtp/federator/service/IdpTokenServiceImpl.java
@@ -75,8 +75,7 @@ public class IdpTokenServiceImpl implements IdpTokenService {
             log.debug("No cached token in Redis for management node {}, fetching from IDP", managementNodeId);
 
             String body =
-                    GRANT_TYPE + EQUALS_SIGN + CLIENT_CREDENTIALS + AMPERSAND + CLIENT_ID + EQUALS_SIGN + idpClientId
-                    + "&client_secret=roZDoPEnk2TtV8kY464f8UfLA0Vi8uB5";
+                    GRANT_TYPE + EQUALS_SIGN + CLIENT_CREDENTIALS + AMPERSAND + CLIENT_ID + EQUALS_SIGN + idpClientId;
 
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(idpTokenUrl))
